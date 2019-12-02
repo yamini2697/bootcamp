667a512a53a8c593f0edd84665421706246c1879
yamini1997 <gundayamini26@gmail.com>
2019-12-02 18:15:13 +0530

    removed comments

--- backend/data.js
+++ backend/data.js
@@ -34,36 +34,7 @@ var response=readFile();
             app.get('/employee',function (req,res) {
                 let respo = JSON.parse(readFile());
                 res.json(respo);
-                //  let remove = req.query.remove;
-                //  if(remove==="salary"){
-                //  var result1=respo.map(function(emp)
-                //  {
-                //     let {salary,...rest} = emp
-                //     return rest
-                //  });
-                //  console.log(result1)
-                //  res.json(result1);
-                //  }
     })
-
-
-
-    // app.get('/employee',function (req,res) {
-    //     const response = JSON.parse(readFile());
-    //     console.log(response);
-    //      let project=req.query.project;
-    //      console.log(project);
-    //      if(project==="p1")
-    //      {
-    //      var emp=function(p){
-    //      var result3=response.filter(e=>e.project===p)
-    //      console.log(result3);
-    //      res.json(result3);
-    //      }
-    //      emp("p1");
-    //     }
-    //  })  
-
 
     app.get('/admin/ctc',function(req,res){
         const resp=JSON.parse(readFile());
@@ -71,7 +42,7 @@ var response=readFile();
         res.json(totalSal)
     })
 
-    
+
 app.listen(5000,()=>{
     console.log("server is running on port 5000");
 })
